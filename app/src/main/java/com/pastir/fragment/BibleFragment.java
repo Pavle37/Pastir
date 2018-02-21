ae93950adb641b4507df93571b6ce29096db5ff4
Uros <uros.rakic@creitive.com>
2018-02-21 17:04:13 +0100

    Single tab design

--- app/src/main/java/com/pastir/fragment/BibleFragment.java
+++ app/src/main/java/com/pastir/fragment/BibleFragment.java
@@ -87,23 +87,21 @@ public class BibleFragment extends BaseFragment {
         @Override
         public Fragment getItem(int position) {
             if (position == 0) {
-                return new BooksFragment();
-            } /*else if (position == 1) {
-                return new PlacesFragment();
+                return BooksFragment.newInstance(getString(R.string.books));
+            } else if (position == 1) {
+                return BooksFragment.newInstance(getString(R.string.chapters));
             } else if (position == 2) {
-                return new FoodFragment();
-            } */ else {
-                return new BooksFragment();
+                return BooksFragment.newInstance(getString(R.string.verses));
+            } else {
+                return BooksFragment.newInstance(getString(R.string.verse));
             }
         }
 
-        // This determines the number of tabs
         @Override
         public int getCount() {
             return 4;
         }
 
-        // This determines the title for each tab
         @Override
         public CharSequence getPageTitle(int position) {
             // Generate title based on item position
@@ -126,23 +124,16 @@ public class BibleFragment extends BaseFragment {
      * A placeholder fragment containing a simple view.
      */
     public static class BooksFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        private static final String ARG_SECTION_NAME = "section_name";
 
         public BooksFragment() {
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static BooksFragment newInstance(int sectionNumber) {
+        public static BooksFragment newInstance(String sectionName) {
             BooksFragment fragment = new BooksFragment();
             Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            args.putString(ARG_SECTION_NAME, sectionName);
             fragment.setArguments(args);
             return fragment;
         }
@@ -151,6 +142,8 @@ public class BibleFragment extends BaseFragment {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_books, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getArguments().getString(ARG_SECTION_NAME));
             return rootView;
         }
     }
